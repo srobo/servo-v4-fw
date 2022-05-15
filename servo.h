59a05ad347faa4f0796efcac05a3b843119a1931
WillB97 <will@rudi.org.uk>
2022-05-15 18:50:42 +0100

    Remove previous code

--- servo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-void servo_init(void);
-
-void servo_set_pos(uint8_t idx, int16_t pos);
-
-uint8_t servo_read();
--- usart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __USART_H
-#define __USART_H
-
-void usart_init(void);
-
-int usart_get_char(void);
-
-#endif /* __USART_H */
